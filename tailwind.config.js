b73309f6fb4c312653f37a6061aa0d3a291911c4
blloop <42186044+blloop@users.noreply.github.com>
2024-06-23 20:10:43 -0700

    configure tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
+    colors: {
+      black: "#000000",
+      white: "#ffffff",
+      gray: "#efeeee",
+      slate: "#c5c6c8",
+      purple: "#6e55f4",
+      purple2: "#77459A",
+      purpleDark: "#4f42d3",
+      aqua: "#00b9d5",
+      darkOverlay: "#000000aa",
+      darkBlueOverlay: "#081123bf",
+      red: "#ce0052",
+      orange: "#faa919",
+      blue: "#1e88e5",
+    },
     extend: {},
   },
   plugins: [],
