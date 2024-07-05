8ab73e294b223ac04c1b4fefaa1e583885567611
blloop <42186044+blloop@users.noreply.github.com>
2024-07-05 00:55:23 -0700

    fix image import

--- src/components/heading.tsx
+++ src/components/heading.tsx
@@ -1,9 +1,11 @@
+import phone from "../assets/phone.png";
+
 export default function Heading() {
   return (
     <div className="h-[calc(100vh-6rem)] p-6 box-border">
       <div className="flex flex-col md:flex-row-reverse items-center bg-grayOverlay size-full box-border rounded-3xl">
         <img
-          src="/phone.png"
+          src={phone}
           className="w-auto h-1/2 md:h-full object-cover relative lg:-left-[5vw]"
         />
         <div className="flex flex-col flex-1 justify-around h-full md:py-8 md:pl-16 text-white">
